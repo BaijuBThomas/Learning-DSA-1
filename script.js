29dd00342b1cb282dfe827dbe9e8f9c483d58782
Baiju Thomas <baijubaby4880@gamil.com>
2025-01-08 01:47:10 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+// fetch('https://dummyjson.com/products')
+// .then((res) =>  res.json())
+// .then((data) => console.log(data))
+
+
+try {
+    console.log(a);
+}catch(err) {
+    console.log(err);
+}
+
+console.log('hello');
+console.log('i am error free');
